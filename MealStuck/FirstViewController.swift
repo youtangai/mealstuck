fee77e1bc783b5995b0a0efb1ef0a41085e63cb4
youtangai <youtanagai@gmail.com>
2016-11-29 14:59:11 +0900

    Initial Commit

---
+++ MealStuck/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MealStuck
+//
+//  Created by 永井陽太 on 2016/11/29.
+//  Copyright © 2016年 永井陽太. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MealStuck/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MealStuck
+//
+//  Created by 永井陽太 on 2016/11/29.
+//  Copyright © 2016年 永井陽太. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
